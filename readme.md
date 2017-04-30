0609539b94475761539807dd1238cdc34550dfce
TJ Draper <tj@buzzingpixel.com>
2017-04-30 17:19:49 -0500

    Reconfigure as NPM module

--- readme.md
+++ readme.md
@@ -2,22 +2,32 @@
 
 While this component is designed with the [BuzzingPixel Fabricator Build Process](https://github.com/tjdraper/buzzing-pixel-fabricator) in mind, it can be used anywhere (in theory).
 
-## `FAB.URL.getQueryString();`
+## Installing
+
+With Fabricator and NPM, simply require this library into your project and restart the Fabricator Grunt build process.
+
+`npm install fab.url --save`
+
+If you are not using Fabricator, you will need to in some manner compile `src/FAB.url.js` into your build process or put it somewhere where you can link it into your projects.
+
+## Usage
+
+### `FAB.URL.getQueryString();`
 
 Gets the raw query string.
 
-## `FAB.URL.getQueryArray();`
+### `FAB.URL.getQueryArray();`
 
 Gets an array of the query string items (split on ampersand).
 
-## `FAB.URL.getQueryObject();`
+### `FAB.URL.getQueryObject();`
 
 Gets an object of the query string with key/values.
 
-## `FAB.URL.getQueryItem('key');`
+### `FAB.URL.getQueryItem('key');`
 
 Checks the query object for the specified key. Returns `null` if key does not exist.
 
-## `FAB.URL.getHash();`
+### `FAB.URL.getHash();`
 
 Gets the hash from the URL. Returns `null` if hash does not exist.
---
+++ src/FAB.url.js
@@ -0,0 +1,84 @@
+/*----------------------------------------------------------------------------*\
+ # Copyright 2017, BuzzingPixel, LLC
+
+ # This program is free software: you can redistribute it and/or modify
+ # it under the terms of the Apache License 2.0.
+ # http://www.apache.org/licenses/LICENSE-2.0
+ \*----------------------------------------------------------------------------*/
+
+// Make sure FAB is defined
+window.FAB = window.FAB || {};
+
+(function(F) {
+    'use strict';
+
+    // Set up some variables
+    var split;
+    var queryObject = {};
+    var queryArray = [];
+
+    // Get query string
+    var queryString = window.location.search;
+    queryString = queryString.split('?')[1];
+
+    // Get hash
+    var hash = window.location.hash;
+    hash = hash.split('#')[1];
+
+    // If there is a query string
+    if (queryString) {
+        // Split on the ampersand seperator
+        queryArray = queryString.split('&');
+
+        // Iterate over number of items in qs
+        for (var i = 0; i < queryArray.length; i++) {
+            // Split the item
+            split = queryArray[i].split('=');
+
+            // Add the item to the qObj
+            queryObject[split[0]] = split[1];
+        }
+    }
+
+    /**
+     * URL API
+     */
+    F.url = {
+        /**
+         * Get query string
+         */
+        getQueryString: function() {
+            return queryString;
+        },
+
+        /**
+         * Get query array
+         */
+        getQueryArray: function() {
+            return queryArray;
+        },
+
+        /**
+         * Get query object
+         */
+        getQueryObject: function() {
+            return queryObject;
+        },
+
+        /**
+         * Get specific query item
+         *
+         * @param {String} key
+         */
+        getQueryItem: function(key) {
+            return queryObject[key] || null;
+        },
+
+        /**
+         * Get hash
+         */
+        getHash: function() {
+            return hash || null;
+        }
+    };
+})(window.FAB);
